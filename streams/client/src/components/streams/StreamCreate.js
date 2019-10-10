861ca370440125838fef55af33b6947d65715830
Fernando Vázquez <fernando.vazquez@adp.com>
2019-10-10 14:39:01 -0300

    Connected to auth and adding redux

---
+++ streams/client/src/components/streams/StreamCreate.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class StreamCreate extends React.Component {
+
+    renderInput({ input, label }) {
+        return (
+            <div className='field'>
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        )
+    }
+
+    onSubmit(formValues) {
+        console.log(formValues)
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form'>
+                <Field name='Title' component={this.renderInput} label='Enter Title' />
+                <Field name='Description' component={this.renderInput} label='Enter Description' />
+                <button className='ui button' type='submit'>Submit</button>
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
